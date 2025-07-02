d8cfb52f225a4cd78c50cbc6886509a3b63e177e
Sulaiman <sulaimancxm@gmail.com>
2025-07-02 18:37:29 +0100

    Delete exercise1part2.py

--- exercise1part2.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Please enter your age: "))
-if age > 16:
-
-    print("You are of age!")
-    age = age + 1
-    print("You are now one year older...")
-else:
-    print("Try again after one year!")
-print("This here belongs to another block")
